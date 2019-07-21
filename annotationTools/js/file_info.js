ff71bc5840afc18f1b42bafc4b3580399d116722
kwon <skgus2624@gmail.com>
2019-07-21 21:40:14 +0900

    test test

--- annotationTools/js/file_info.js
+++ annotationTools/js/file_info.js
@@ -266,7 +266,7 @@ function file_info() {
     html_str4 += '</div>';
                 
         //var html_str2 = '<font size="4"><b>Scroll up to see the entire image</b></font>&#160;&#160;&#160;<font size="3">Describe the painting</font><br /><textarea id="mt_comments_textbox" name="mt_comments_texbox" cols="94" nrows="3" />';
-		//$('#mt_feedback').append(html_str2);
+		$('#mt_feedback').append(html_str4);
 
                 if(global_count >= mt_N) document.getElementById('mt_submit').disabled=false;
             }
