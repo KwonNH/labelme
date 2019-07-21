14f86a8688a194ed404a61fafc6dbef390c66106
kwon <skgus2624@gmail.com>
2019-07-21 21:54:30 +0900

    test test

--- annotationTools/js/file_info.js
+++ annotationTools/js/file_info.js
@@ -268,9 +268,11 @@ function file_info() {
         //var html_str2 = '<font size="4"><b>Scroll up to see the entire image</b></font>&#160;&#160;&#160;<font size="3">Describe the painting</font><br /><textarea id="mt_comments_textbox" name="mt_comments_texbox" cols="94" nrows="3" />';
 		$('#mt_submit').append(html_str4);
 */
-$('#zoomin').attr("onclick","javascript:main_media.Zoom(1.15)");
+ $('#zoomin').attr("onclick","javascript:main_media.Zoom(1.15)");
   $('#zoomout').attr("onclick","javascript:main_media.Zoom(1.0/1.15)");
-InitializeAnnotationTools('label_buttons_drawing','main_media');
+  $('#fit').attr("onclick","javascript:main_media.Zoom('fitted')");
+
+  
                 if(global_count >= mt_N) document.getElementById('mt_submit').disabled=false;
             }
         }
