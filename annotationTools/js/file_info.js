b935d443e4e8b0b76e13ca45a4680729a9ab0c81
kwon <skgus2624@gmail.com>
2019-07-24 20:49:34 +0900

    test test

--- annotationTools/js/file_info.js
+++ annotationTools/js/file_info.js
@@ -253,14 +253,14 @@ function file_info() {
 
 		$('#mt_submit_form').append(html_str3);
 
-		var html_str4 = '<div id= "zoom" class="zoommenu"><br></br> \
-        <button id="zoomin" class="labelBtnDraw" type="button" title="Start Polygon" onclick="javascript:main_media.Zoom(1.15)" > \
+		var html_str4 = '<div id= "zoom" class="zoommenu"><br>Zoom In/Out Tools</br> \
+        <button id="zoomin" class="labelBtnDraw" type="button" title="Zoom In" onclick="javascript:main_media.Zoom(1.15)" > \
         <img id="polygonModeImg" src="Icons/zoomin.png"  width="20" height="23" /> \
         </button> \
-        <button id="zoomout" class="labelBtnDraw" type="button" title="Delete last segment" onclick="javascript:main_media.Zoom(1.0/1.15)" > \
+        <button id="zoomout" class="labelBtnDraw" type="button" title="Zoom Out" onclick="javascript:main_media.Zoom(1.0/1.15)" > \
         <img src="Icons/zoomout.png" width="20" height="23" /> \
         </button> ';
-        html_str4 += ' <button id="fit" class="labelBtnDraw" type="button" title="Start bounding box" onclick="javascript:main_media.Zoom(\'fitted\')" > \
+        html_str4 += ' <button id="fit" class="labelBtnDraw" type="button" title="Fit Image" onclick="javascript:main_media.Zoom(\'fitted\')" > \
         <img src="Icons/fitscreen.png"  width="20" height="23"  /> \
         </button> ';
     html_str4 += '</div>';
